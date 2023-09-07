6b141082714cd23b7ccb32faa2f83b42e4b654ae
Roman <roman.legeza@gmail.com>
2023-09-07 12:34:36 -0700

    Header/Footer redesigned

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import { Grid, ThemeProvider } from '@mui/material';
-import Box from '@mui/material/Box';
-import Item from '@mui/material/Grid';
-import '@mui/material/styles';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import { navItems } from './constants';
-import { appTheme } from './theme';
-
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider theme={appTheme}>
-        <Header />
-        <Grid container>
-          <Grid item xs={12} md={12}>
-            <Item>
-              <Box display="flex" justifyContent="center" alignItems="center" sx={{ minWidth: 300 }}>
-                <Routes>
-                  {navItems.map((item, index) => {
-                    return <Route key={index} path={item.path} element={<item.component />} />;
-                  })}
-                </Routes>
-              </Box>
-            </Item>
-          </Grid>
-        </Grid>
-        <Footer />
-      </ThemeProvider>
-    </Router>
+    <div className="App">
+      <h1>Homepage</h1>
+    </div>
   );
 }
 
